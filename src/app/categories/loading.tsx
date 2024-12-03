c8714512e33480ea2371f27095040173f17e09d5
arsian <marshia370@gmail.com>
2024-12-04 01:40:05 +0330

    .

--- src/app/categories/loading.tsx
+++ src/app/categories/loading.tsx
@@ -1,7 +1,7 @@
 'use client'
 export default function Loading() {
     return (
-        <div className="">
+        <div>
             <div className="text-center py-40">
                 <div className="m-auto animate-spin rounded-full h-16 w-16 border-t-4 border-[#cbaa6a]"></div>
             </div>
